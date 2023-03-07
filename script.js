3b6fe31235f10d6b2c43f88ab0f5eef3cadd75f1
Chemiker1 <66435379+Chemiker1@users.noreply.github.com>
2023-03-07 19:05:37 +0100

    Add playRound function

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function getComputerChoice() {
     // Returns randomly "Rock", "Paper" or "Scissors"
+
     let randomInt = Math.floor(Math.random() * 3); // 0, 1 or 2
+
     switch (randomInt) {
         case 0:
             return "Rock";
@@ -8,5 +10,24 @@ function getComputerChoice() {
             return "Paper";
         default:
             return "Scissors";
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    let gameString = `${playerSelection} against ${computerSelection}.`;
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return gameString + " It's a tie!";
+    } else if (
+        playerSelection === "rock" && computerSelection === "paper" ||
+        playerSelection === "paper" && computerSelection === "scissors" ||
+        playerSelection === "scissors" && computerSelection === "rock"
+    ) {
+        return gameString + " You lost!";
+    } else {
+        return gameString + " You won!";
     }
 }
